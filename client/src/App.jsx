378392cdcd395523cff822debf2131d31d8fc1cb
Oğuzhan Güç <oguzguc44@gmail.com>
2023-06-17 08:31:00 +0300

    modified:   client/src/App.css     -Error Page css added
    modified:   client/src/App.jsx
        -Protected routes defined
    
    modified:   client/src/index.css
        -Fonts added
    
    modified:   client/src/main.jsx
        Application wrapped with Redux Provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,40 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { setToken } from './redux/slice/postSlice'
 
 import './App.css'
 import Home from './pages/Home'
 import About from './pages/About'
 import Login from './auth/Login'
 import Create from './pages/Create'
+import Error from './pages/Error'
+
+import MainRoutes from './routes/MainRoutes'
+import SinglePost from './pages/SinglePost'
 
 function App() {
+  const { token } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  const storageToken = localStorage.getItem("token");
+  if (storageToken && !token) {
+    dispatch(setToken(storageToken));
+  }
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route exact path='/' element={<Navigate to={"home"} />} /> 
+      <Route path='/' element={<MainRoutes />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {
+          storageToken && <Route path="create" element={<Create />} />
+        }
+        <Route path='/post/:id' element={<SinglePost/>}/>
+        <Route path="*/" element={<Error/>} />
+      </Route>
       <Route path="/login" element={<Login />} />
-      <Route path="/create" element={<Create />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   )
 }
